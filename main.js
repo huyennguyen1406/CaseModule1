19561b72a67d8b8ac05c6fe0882fb5c7153ca2a6
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2021-11-26 15:44:15 +0700

    update main

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ const SQ = squareSize = 40;
 const VACANT = "WHITE" //color of an empty space
 
 //draw a square
-function drawSquare(x,y,color) {
+function drawSquare(x, y, color) {
     ctx.fillStyle = color;
-    ctx.fillRect(x*SQ, y*SQ, SQ, SQ);
+    ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
 
     strokeStyle = "BLACK";
-    ctx.strokeRect(x*SQ, y*SQ, SQ, SQ);
+    ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
 
 // create the board
@@ -51,7 +51,7 @@ function randomPiece(){
     let r = randomN = Math.floor(Math.random() * PIECES.length) // 0 -> 6
     return new Piece(PIECES[r][0], PIECES[r][1])
 }
-// initate a piece
+// initiate a piece
 
 let p = randomPiece();
 
@@ -72,7 +72,7 @@ function Piece(tetromino,color){
 
 Piece.prototype.fill = function(color){
     for(r = 0; r < this.activeTetromino.length; r++){
-        for(c = 0;c < this.activeTetromino.length;c++){
+        for(c = 0; c < this.activeTetromino.length;c++){
             //we draw only occupied squares
             if(this.activeTetromino[r][c]){
                 drawSquare(this.x + c, this.y + r,color);
@@ -139,7 +139,7 @@ Piece.prototype.rotate = function(){
     if(!this.collision(kick,0,nextPattern)) {
         this.unDraw();
         this.x += kick;
-        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length; // (0+1)%4 => 1
+        this.tetrominoN = (this.tetrominoN + 1) % this.tetromino.length; // (0 + 1) % 4 => 1
         this.activeTetromino = this.tetromino[this.tetrominoN];
         this.draw();
     }
@@ -148,12 +148,12 @@ let score = 0;
 
 Piece.prototype.lock = function(){
     for(r = 0; r < this.activeTetromino.length; r++){
-        for(c = 0;c < this.activeTetromino.length;c++){
+        for(c = 0; c < this.activeTetromino.length; c++){
             //we skip the vacant squares
             if(!this.activeTetromino[r][c]){
                 continue;
             }
-            console.log(this.y+r)
+            console.log(this.y + r)
             //pieces to lock on top = game over
             if(this.y + r < 0){
                 alert("Game Over");
@@ -162,7 +162,7 @@ Piece.prototype.lock = function(){
                 break;
             }
             //we lock the piece
-            board[this.y+r][this.x + c] = this.color;
+            board[this.y + r][this.x + c] = this.color;
         }
     }
     //remove full rows
@@ -175,12 +175,12 @@ Piece.prototype.lock = function(){
             //if the row is full
             // we move down all the rows above it
             for(y=r;y > 1; y--){
-                for(c = 0 ;c < COL;c++){
-                    board[y][c] = board[y-1][c];
+                for(c = 0; c < COL; c++){
+                    board[y][c] = board[y - 1][c];
                 }
             }
             //the top row board[0][..] has no row above it
-            for(c = 0; c < COL;c++){
+            for(c = 0; c < COL; c++){
                 board[0][c] = VACANT;
             }
             //increment the score
@@ -241,7 +241,6 @@ function CONTROL(event){
     }
 }
 //drop the pieces every 1sec
-
 let dropStart = Date.now();
 let gameOver = false;
 function drop() {
